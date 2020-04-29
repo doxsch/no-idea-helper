7ad29ff2c4b68d02fa7b6f129a77b38a50e27629
doxsch <28098153+doxsch@users.noreply.github.com>
2020-04-29 22:34:51 +0200

    setup initial styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+      <main>
+          <div className='card'>
+              <h1>No idea what you want to program today?</h1>
+
+          </div>
+      </main>
+      <footer>created with ♥ from doxsch</footer>
     </div>
   );
 }
