54d5de139d97bec5488c9495c16fd0b1a7ae6f7c
doxsch <28098153+doxsch@users.noreply.github.com>
2020-04-30 23:11:03 +0200

    redesigned whole app added button component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
     <div className="App">
       <header>
+          <h1>No idea what you want to program today?</h1>
+          <p>click on <span>generate</span> to get a public api for
+              your next project.</p>
       </header>
       <main>
-          <div className='card'>
-              <h1>No idea what you want to program today?</h1>
 
-          </div>
+
+              <div>
+                <Button text='generate'></Button>
+              </div>
+
       </main>
       <footer>created with ♥ from doxsch</footer>
     </div>
